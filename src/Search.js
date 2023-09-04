b5f91e3b53e2f23e56b0d14011ad75031999626d
Matheus Sanabria <codificadoresdenegocios@gmail.com>
2023-09-04 20:55:28 -0300

    A4
    Servindo dados da forma correta

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 function Search(props) {
+
+    const [city,setCity] = useState('');
 
     function detectSearch(e){
         e.preventDefault();
+        // setCity('');
+
         let currentValue = document.querySelector('input[name=detectSearch').value;
         
         // alert(currentValue);
@@ -14,28 +19,65 @@ function Search(props) {
         .then(data => {
             // destruction, estamos criando dicionario com os dados de resposta da requisicao
             const {main, name, sys, weather} = data;
-            if(sys !== undefined)
+            if(sys !== undefined){
                 console.log(sys.country);
-            if(weather !== undefined)
-                console.log(weather[0]['description']);
+
+                if(weather !== undefined){
+                    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+                    weather[0]["icon"]}.svg`;
+                    setCity(`
+
+                    <div>
+    
+                    <p>Temperatura: ${main.temp}C°</p>
+    
+                    <p>Pais: ${sys.country}</p>
+    
+                    <p>Cidade: ${name}</p>
+    
+                    <p>Descrição: ${weather[0]['description']}</p>
+    
+                    <img src="${icon}" />
+    
+                    </div>
+    
+                    `);
+                    console.log(weather[0]['description']);
+
+                }
+            }else{
+                setCity('');
+            }
             console.log(data)
         })
     }
   return (
     <div className="search">
-        <div className="container"> 
-            <h2>Search for the city you want to know the forecast 🌞</h2>
-            <form onSubmit={(e)=>detectSearch(e)}>
-                <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder={props.placeholder} onKeyUp={detectSearch} name="detectSearch" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-                    
-                    <div className="input-group-append">
-                        <button className="btn btn-success" type="submit">Search</button>
+        <div className='searchWeather'>
+            <div className="container">
+                <h2>Search for the city you want to know the forecast 🌞</h2>
+                <form onSubmit={(e)=>detectSearch(e)}>
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder={props.placeholder} onKeyUp={detectSearch} name="detectSearch" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                        
+                        <div className="input-group-append">
+                            <button className="btn btn-success" type="submit">Search</button>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
-    </div>
+                </form>
+            </div>{/* container */}
+        </div>{/* searchWeather */}
+        <div className='SearchResult'>
+
+        </div>{/* SearchResult */}
+
+        {
+            (city !== '')?
+                <div dangerouslySetInnerHTML={{__html: city}}/>:
+                <div>Sem resultados</div>
+        }
+
+    </div>/* search */
   );
 }
 
